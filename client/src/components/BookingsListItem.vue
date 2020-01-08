266ad3af6f6a5f15c46a9734f59e73b83a183ce4
Debbie <debiurquhart@live.co.uk>
2020-01-08 15:48:41 +0000

    adds update button

--- client/src/components/BookingsListItem.vue
+++ client/src/components/BookingsListItem.vue
@@ -6,6 +6,9 @@
     <button type="button" class="delete-btn" v-on:click="deleteBooking">
       Delete
     </button>
+    <button class="delete-btn" v-on:click="updateBooking">
+      update check in
+    </button>
   </tr>
 </template>
 
@@ -21,23 +24,27 @@ export default {
         eventBus.$emit("booking-deleted", this.booking._id)
       );
     }
+  },
+
+  updateBooking() {
+    BookingService.updateBooking(this.booking).then(() =>
+      eventBus.$emit("booking-updated", this.booking)
+    );
   }
 };
 </script>
 
 <style lang="css" scoped>
-
 td {
   padding: 3px 20px 10px;
-  font-family:arial, san-serif;
+  font-family: arial, san-serif;
 }
-
 
 .delete-btn {
-  background-color:#00EAFF;
-  border:none;
+  background-color: #00eaff;
+  border: none;
   color: black;
-  padding:5px;
+  padding: 5px;
+  margin: 5px;
 }
-
 </style>
