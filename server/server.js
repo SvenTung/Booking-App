4b36ad2be4f1c655b49bcd70372b333713ca1ee5
user <user@users-MacBook-Pro-9.local>
2020-01-08 13:10:35 +0000

    add all options

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-This is a test (server.js)
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router');
+const cors = require('cors');
+
+app.use(cors())
+app.use(parser.json())
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if(err){
+    console.log(err);
+  }
+
+  const db = client.db('bookings_app');
+  const booksingsCollection = db.collection('bookings');
+  const bookingRouter = createRouter(bookingsCollection)
+  app.use('/api/bookings', bookingsRouter);
+
+  app.listen(3000, function(){
+    console.log(`app listening on port ${this.address().port}`);
+  })
+})
