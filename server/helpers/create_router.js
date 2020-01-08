a451bf4e9e0406f51309777d1f823038d847ade3
chad-tung <sven.tung@gmail.com>
2020-01-08 13:10:44 +0000

    Git pulled Debbie's changes and added the create-router file

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,1 +1,68 @@
-// Test create_router.js
+const express = require('express')
+const ObjectId = require('mongodb').ObjectID;
+
+const createRouter = function (collection) {
+
+  const router = express.Router();
+
+  router.get('/', (req, res) => {
+    collection.find().toArray()
+    .then(docs => res.json(docs))
+    .catch((error) => {
+      console.error(error);
+      res.status(500);
+      res.json({status: 500, error: error})
+    })
+  });
+
+  router.get('/:id', (req, res) => {
+    const id = ObjectId(req.params.id)
+    collection.findOne({_id: id})
+    .then(docs => res.json(docs))
+    .catch((error) => {
+      console.error(error)
+      res.status(500)
+      res.json({status: 500, error: error})
+    })
+  });
+
+  router.post('/', (req, res) => {
+    collection.insertOne(req.body)
+    .then(docs => res.json(docs.op[0]))
+    .catch((error) => {
+      console.error(error)
+      res.status(500)
+      res.json({status: 500, error: error})
+    })
+  });
+
+  router.delete('/:id', (req, res) => {
+    const id = ObjectId(req.params.id)
+    collection.deleteOne({_id: id})
+    .then(result => res.json(result))
+    .catch((error) => {
+      console.error(error)
+      res.status(500)
+      res.json({status: 500, error: error})
+    })
+  });
+
+  router.put('/:id', (req, res) => {
+    const id = ObjectId(req.params.id)
+    const updatedData = req.body
+    collection.findOneAndUpdate(
+      {_id: id},
+      {$set: updatedData},
+      {returnOriginal: false})
+    .then(result => res.json(result.value))
+    .catch((error) => {
+      console.error(error)
+      res.status(500)
+      res.json({status: 500, error: error})
+    })
+  });
+
+  return router;
+
+};
+module.exports = createRouter;
