ef8b71b86c49cc06ac6ee93266e03496df44c733
Debbie <debiurquhart@live.co.uk>
2020-01-08 13:40:36 +0000

    adds booking form

---
+++ client/src/components/BookingsForm.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <label for="name">Customer Name</label>
+  <input type="text" id="name" v-model="name" required />
+
+  <label for="email">Customer Email</label>
+  <input type="email" v-model="email" required />
+
+  <label for="checkedId">Is the Customer checked in?</label>
+  <input type="checkbox" value="true" v-model="checkedIn" />
+</template>
+
+<script>
+export default {
+  name: "bookingForm",
+  data() {
+    return {
+      name: "",
+      email: "",
+      checkedIn: false
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
