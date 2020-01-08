b0bb4619aefb909dc737aea96cbf50ba6e2923dc
Debbie <debiurquhart@live.co.uk>
2020-01-08 12:12:54 +0000

    adds js objects to seeds file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,1 +1,34 @@
-this is debbies test
+use customers;
+db.dropDatabase();
+
+db.bookings.insertMany([
+    {
+        Name: "Mark",
+        Email: "mark@mail.com",
+        CheckedIn: true
+    },
+
+    {
+        Name: "Debbie",
+        Email: "Debbie@email.co.uk",
+        CheckedIn: true
+    },
+
+    {
+        Name: "Sven",
+        Email: "sven@mailbox.co.uk",
+        CheckedIn: false
+    },
+
+    {
+        Name: "Brad",
+        Email: "brad@email.com",
+        CheckedIn: true
+    },
+
+    {
+        Name: "Kyle",
+        Email: "kyle@mail.co.uk",
+        CheckedIn: false
+    },
+])
